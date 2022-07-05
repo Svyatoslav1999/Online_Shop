9d5d953df4828e88aca2ada907ea12b7c40da2d5
Svyatoslav <Svyatoslav1999@ukr.net>
2022-07-05 11:32:33 +0200

    startup

---
+++ src/main/java/com/example/online_shop/entity/ProductEntity.java
@@ -0,0 +1,21 @@
+package com.example.online_shop.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Products")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 25, nullable = false)
+    private String productName;
+
+    @Column(columnDefinition = "DOUBLE(5,2) not null")
+    private Double productPrice;
+
+
+
+}
